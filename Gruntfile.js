d52c89ed3ff66f80dd2cf45c609d63317344400b
suhas-shetty <suhas.shetty022@gmail.com>
2016-09-12 17:43:16 +0530

    1)added angular and angular material in bower.json 2)added grunt-contrib-watch and grunt-wiredep in package.json 3)bower dependencies related changes in gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,19 @@
 module.exports = function(grunt){
-	grunt.registerTask('testgrunt', function(){
-		console.log('grunt tested');
+	grunt.loadNpmTasks('grunt-wiredep');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.initConfig({
+		wiredep: {
+			task: {
+				src: ['index.html']
+			}
+		},
+		watch: {
+			bowerdep:{
+				files: ['scripts/bower_components/**/*'],
+				tasks: ['wiredep']
+			}
+		}
 	});
+	grunt.registerTask('default', ['watch']);
 };
